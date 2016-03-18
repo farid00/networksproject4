5b36ba3cbf9938bdc342f937ea866ff524315a85
farid00 <brandman.m@husky.neu.edu>
2016-03-18 01:05:30 -0400

    login is giving a correct session cookie but for someone reason the next get is being a little bitch

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python 
 import sys 
 import socket
+import textwrap
 from HTMLParser import HTMLParser
 #Parse out links from HTTP response
 class linkParser(HTMLParser):
@@ -23,22 +24,69 @@ def parse_cookies(response):
 			cookie_value = cookie[1]
 			cookie_dictionary[cookie_name] = cookie_value
 			print cookie
+	return cookie_dictionary
 
-def login():
+def make_login_string(cookie_string, csrfmiddlewaretoken):
 	username = "001968841"
 	password = "LG56YYQK"
+	login_string = """
+					POST /accounts/login/ HTTP/1.1
+					Host: fring.ccs.neu.edu
+					Referer: http://fring.ccs.neu.edu/accounts/login/?next=/fakebook/b
+					Content-Type: application/x-www-form-urlencoded
+					Content-Length: 91
+					Origin: http://fring.ccs.neu.edu
+					Cookie: %s 
+
+					username=%s&password=%s&csrfmiddlewaretoken=%s&next=
+
+					"""
+	print textwrap.dedent(login_string % (cookie_string, username, password, csrfmiddlewaretoken))
+	return textwrap.dedent(login_string % (cookie_string, username, password, csrfmiddlewaretoken))
 def connect():
 	s = socket.socket()
 	s.connect(("fring.ccs.neu.edu", 80))
 	return s 
+
+
+def make_cookie_string(cookie_dictionary):
+	myCS = ""
+	for key, value in cookie_dictionary.iteritems():
+		current_str = "%s=%s; "
+		current_str = current_str % (key, value)
+		myCS += current_str
+	return myCS[:-1]
+
+def make_get_request(cookie_string, url_to_get, socket):
+	request_string = """
+					GET /fakebook/ HTTP/1.1
+					Host: fring.ccs.neu.edu
+					Cookie: %s
+
+					"""
+	request_string = textwrap.dedent(request_string % (cookie_string))
+	print request_string
+	socket.sendall(request_string)
+	response = socket.recv(4096)
+	return response
+
 def main():
 	parser = linkParser()
 	s = connect()
 	home_page = "GET /accounts/login/?next=/fakebook/ HTTP/1.1\r\nHost: fring.ccs.neu.edu\r\n\r\n"
 	s.send(home_page)
 	response = s.recv(4096)
-	parse_cookies(response)
-	parser.feed(response)
+	print response
+	cookies = parse_cookies(response)
+	links = parser.feed(response)
+	cookie_string = make_cookie_string(cookies)
+	login_string = make_login_string(cookie_string, cookies['csrftoken'])
+	s.send(login_string)
+	response = s.recv(4096)
+	print response
+	cookies = parse_cookies(response)
+	cookie_string = make_cookie_string(cookies)
+	print make_get_request(cookie_string, '/fakebook/', s)
 
 if __name__ == "__main__":
 	main()
