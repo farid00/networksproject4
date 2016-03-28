cb8abfe248b5befc89a90d907d0ce4acbbdbf51c
farid00 <brandman.m@husky.neu.edu>
2016-03-28 18:27:12 -0400

    thread locking

--- crawler-objective.py
+++ crawler-objective.py
@@ -9,7 +9,23 @@ import time
 import binascii
 import Queue
 import threading
+import select
+cond = threading.Condition()
 Flags = []
+SESSION = ''
+
+class SetQueue(Queue.Queue):
+
+    def _init(self, maxsize):
+        self.maxsize = maxsize
+        self.queue = set()
+
+    def _put(self, item):
+        self.queue.add(item)
+
+    def _get(self):
+        return self.queue.pop()
+
 class linkParser(HTMLParser):
     def __init__(self, LinksToVisit, LinksVisitted, Flags):
         self.LinksToVisit = LinksToVisit
@@ -51,8 +67,10 @@ class WebCrawler():
         self.parser = linkParser(LinksToVisit=self.LinksToVisit, LinksVisitted=self.LinksVisitted, Flags=self.Flags)
         self.sock = socket.socket()
         self.sock.connect(("fring.ccs.neu.edu", 80))
+        self.sock.setblocking(0)
 
     def crawl(self):
+        global SESSION
         cookies = {}
 
         home_page = "GET /accounts/login/ HTTP/1.1\r\nHost: fring.ccs.neu.edu\r\n\r\n"
@@ -62,12 +80,12 @@ class WebCrawler():
             response = self.compile_response(response)
             cookies = self.parse_cookies(response)
             cookie_string = self.make_cookie_string(cookies)
-
         login_string = self.make_login_string(cookie_string, cookies.get('csrftoken'))
         self.sock.send(login_string)
         response = self.safe_recv(4096)
         cookies = self.parse_cookies(response)
         cookie_string = self.make_cookie_string(cookies)
+        SESSION = cookie_string
         resp, statuscode = self.make_get_request(url_to_get='/fakebook/', cookie_string=cookie_string)
         self.parser.feed(resp)
         is_closed = False
@@ -78,8 +96,11 @@ class WebCrawler():
             print 'NO LINKS AT START COULD NOT CRAWL'
 
         while not self.LinksToVisit.empty():
-            NextUrl = str(self.LinksToVisit.get(0))
-            print "{} {} {}".format(NextUrl.ljust(40), str(self.LinksToVisit.qsize()).ljust(40), str(self.LinksVisitted.qsize()).ljust(40))
+            try:
+                NextUrl = str(self.LinksToVisit.get(0))
+            except:
+                continue
+            print "*****{} {} {}*****".format(NextUrl.ljust(40), str(self.LinksToVisit.qsize()).ljust(40), str(self.LinksVisitted.qsize()).ljust(40))
 
             #cookie string shouldnt change except maybe the csrf will but idk if thats neccesary
             (http_response, sc) = self.make_get_request(url_to_get=NextUrl, cookie_string=cookie_string)
@@ -94,8 +115,11 @@ class WebCrawler():
             elif int(sc) == 301:
                 # TODO: this is a kind of wonky algorithm but it works
                 redirecturl = http_response.split("Location: ",1)[1].split()[0]
+                cond.acquire()
                 if redirecturl not in self.LinksVisitted.queue and redirecturl not in self.LinksToVisit:
                     self.LinksToVisit.insert(redirecturl)
+                    cond.notify()
+                    cond.release()
             elif int(sc) == 403:
                 print '403 error, abandoning url'
 
@@ -115,7 +139,7 @@ class WebCrawler():
         response = ""
         while True:
             if length_left > 0:
-                new_response = self.safe_recv(length_left)
+                new_response = self.safe_recv(4096)
                 length_left = length_left - len(new_response)
                 # print str(len(new_response)) + ' new length'
                 # print str(length_left) + ' length left'
@@ -128,6 +152,7 @@ class WebCrawler():
         # print repr(response)
         # print "$$$$$$$$$$$$$$$$$$$$$$$$$"
         if response.find('chunked') > 0:
+
             current_response = response
             reassembled_response = ""
             while True:
@@ -135,13 +160,12 @@ class WebCrawler():
                 #     m = re.search(r"(?<=\r\n\r\n)[0-9A-Fa-f]*(?=\r\n)", current_response)
                 # else:
                 m = re.findall(r"(?<=\r\n)[0-9A-Fa-f]{3}(?=\r\n)|[0-9A-Fa-f]{1}(?=\r\n\r\n)", current_response)
-                print m
-                if '0' in m:
-                    return reassembled_response
                 #add 2 because of the final carriage return and line return on chunk
                 #split on the csrf number
                 body_response = re.sub(r"(\r\n[0-9A-Fa-f]{3}\r\n)", '', current_response)
                 reassembled_response += body_response
+                if '0' in m:
+                    return reassembled_response
                 current_response = self.safe_recv(4096)
         else:
             response = response
@@ -224,7 +248,8 @@ class WebCrawler():
 
     def safe_recv(self, length):
         try:
-            return self.sock.recv(length)
+            read, write, other = select.select([self.sock], [], [])
+            return read[0].recv(length)
         except Exception as e:
             # TODO: does this make sense? also check lack of use in recvall method im not sure it will work there.
             print 'connection reset by peer error'
@@ -242,8 +267,8 @@ def print_flags():
     else:
         print 'NO FLAGS FOUND'
 def main(argv):
-    LinksToVisit = Queue.Queue()
-    LinksVisitted = Queue.Queue()
+    LinksToVisit = SetQueue()
+    LinksVisitted = SetQueue()
     if len(argv) >= 2:
         username = argv[0]
         password = argv[1]
@@ -256,24 +281,40 @@ def main(argv):
         password = "66H5YT05"
 
         # Matt's credientials
-        # username = "001968841"
-        # password = "LG56YYQK"
+        #username = "001968841"
+        #password = "LG56YYQK"
         crawler = WebCrawler(username=username, password=password, LinksVisitted = LinksVisitted, LinksToVisit=LinksToVisit)
         crawler2 = WebCrawler(username=username, password=password, LinksVisitted = LinksVisitted, LinksToVisit=LinksToVisit)
         crawler3 = WebCrawler(username=username, password=password, LinksVisitted = LinksVisitted, LinksToVisit=LinksToVisit)
         crawler4 = WebCrawler(username=username, password=password, LinksVisitted = LinksVisitted, LinksToVisit=LinksToVisit)
+        crawler5 = WebCrawler(username=username, password=password, LinksVisitted = LinksVisitted, LinksToVisit=LinksToVisit)
+        crawler6 = WebCrawler(username=username, password=password, LinksVisitted = LinksVisitted, LinksToVisit=LinksToVisit)
+        crawler7 = WebCrawler(username=username, password=password, LinksVisitted = LinksVisitted, LinksToVisit=LinksToVisit)
+        crawler8 = WebCrawler(username=username, password=password, LinksVisitted = LinksVisitted, LinksToVisit=LinksToVisit)
         t1 = threading.Thread(target=crawler.crawl, args=[])
         t2 = threading.Thread(target=crawler2.crawl, args=[])
         t3 = threading.Thread(target=crawler3.crawl, args=[])
         t4 = threading.Thread(target=crawler4.crawl, args=[])
+        t5 = threading.Thread(target=crawler5.crawl, args=[])
+        t6 = threading.Thread(target=crawler6.crawl, args=[])
+        t7 = threading.Thread(target=crawler7.crawl, args=[])
+        t8 = threading.Thread(target=crawler8.crawl, args=[])
         t1.start()
         t2.start()
         t3.start()
         t4.start()
+        t5.start()
+        t6.start()
+        t7.start()
+        t8.start()
         t1.join()
         t2.join()
         t3.join()
         t4.join()
+        t5.join()
+        t6.join()
+        t7.join()
+        t8.join()
         print_flags()
 
 if __name__ == "__main__":
